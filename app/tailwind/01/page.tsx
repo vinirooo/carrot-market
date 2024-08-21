a42f3bd11266d92babc3a3c130a6c7cb16efdbe9
vinirooo <vinirooo@gmail.com>
2024-08-21 21:41:52 +0900

    tailwind 두번째 예제 실습

--- app/tailwind/01/page.tsx
+++ app/tailwind/01/page.tsx
@@ -1,4 +1,4 @@
-export default function Tailwind1() {
+export default function Tailwind01() {
   return (
     <main className="bg-gray-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
       <div className="bg-white shadow-md p-5 rounded-3xl w-full max-w-screen-sm dark:bg-gray-500">
--- app/tailwind/02/page.tsx
+++ app/tailwind/02/page.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function Tailwind02() {
   return (
     <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
       <div className="bg-white shadow-md p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-2">
