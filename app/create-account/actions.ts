505a0f5e665ab1a7208f155ee57d9f8aaa36fe39
vinirooo <vinirooo@gmail.com>
2024-08-29 23:34:31 +0900

    6-2 Refinement
    feat: Improve form validation and error handling
    
    The code changes in this commit improve the form validation and error handling in the create account feature. It introduces the use of the Zod library for schema validation and adds custom error messages for specific validation rules. This enhancement ensures that users receive clear and informative error messages when submitting invalid form data.

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -2,12 +2,33 @@
 
 import { z } from "zod";
 
-const formSchema = z.object({
-  username: z.string().min(3).max(20),
-  email: z.string().email(),
-  password: z.string().min(6).max(20),
-  confirmPassword: z.string().min(6).max(20),
-});
+const checkUsername = (username: string) => username.includes("admin");
+const checkConfirmPassword = ({
+  password,
+  confirmPassword,
+}: {
+  password: string;
+  confirmPassword: string;
+}) => password === confirmPassword;
+
+const formSchema = z
+  .object({
+    username: z
+      .string()
+      .min(3, "username은 3자보다 길어야 합니다.")
+      .max(20, "username은 20자보다 짧아야 합니다.")
+      .refine(checkUsername, "username은 admin을 포함할 수 없습니다."),
+    email: z.string().email(),
+    password: z
+      .string()
+      .min(6, "비밀번호는 6자 이상이어야 합니다.")
+      .max(20, "비밀번호는 20자 이하여야 합니다."),
+    confirmPassword: z.string(),
+  })
+  .refine(checkConfirmPassword, {
+    message: "password와 confirmPassword가 일치하지 않습니다.",
+    path: ["confirmPassword"],
+  });
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
