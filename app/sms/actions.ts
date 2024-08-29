a41ce7c2794fcddc2303b72963e59d293745ae8e
vinirooo <vinirooo@gmail.com>
2024-08-30 00:53:23 +0900

    6-7 coerce

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,10 +1,17 @@
 "use server";
 
-export async function smsVerification(prevState: any, formData: FormData) {
+import { z } from "zod";
+import validator from "validator";
+
+const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+const tokenSchema = z.coerce.number().min(100000).max(999999);
+
+export async function smsLogin(prevState: any, formData: FormData) {
   const data = {
     phone: formData.get("phone"),
     token: formData.get("token"),
   };
 
-  console.log(data);
+  console.log(typeof data.token);
+  console.log(typeof tokenSchema.parse(data.token));
 }
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -1,20 +1,27 @@
+"use client";
+
 import Button from "@/components/button";
 import Input from "@/components/input";
+import { smsLogin } from "./actions";
+import { useFormState } from "react-dom";
 
 export default function SMSPage() {
+  const [state, dispatch] = useFormState(smsLogin, null);
   return (
     <div className="flex flex-col gap-10 px-6 py-8">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">SMS Login</h1>
         <p className="text-xl">Verify your phone number.</p>
       </div>
-      <form className="flex flex-col gap-3">
-        <Input name="phone" type="number" placeholder="Phone number" required />
+      <form action={dispatch} className="flex flex-col gap-3">
+        <Input name="phone" type="text" placeholder="Phone number" required />
         <Input
           name="token"
           type="number"
           placeholder="Verification code"
           required
+          min={100000}
+          max={999999}
         />
         <Button text="Verify" />
       </form>
