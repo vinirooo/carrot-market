5d0330acc9eef14c43a6e93f5e38297a319f104a
vinirooo <vinirooo@gmail.com>
2024-08-30 02:22:19 +0900

    6-8 sms validation

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -2,16 +2,46 @@
 
 import { z } from "zod";
 import validator from "validator";
+import { redirect } from "next/navigation";
 
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine(
+    (phone) => validator.isMobilePhone(phone, "ko-KR"),
+    "전화번호 형식이 잘못되었습니다.",
+  );
+
 const tokenSchema = z.coerce.number().min(100000).max(999999);
 
-export async function smsLogin(prevState: any, formData: FormData) {
-  const data = {
-    phone: formData.get("phone"),
-    token: formData.get("token"),
-  };
+interface ActionState {
+  token: boolean;
+}
 
-  console.log(typeof data.token);
-  console.log(typeof tokenSchema.parse(data.token));
+export async function smsLogin(prevState: ActionState, formData: FormData) {
+  const phone = formData.get("phone");
+  const token = formData.get("token");
+  if (!prevState.token) {
+    const result = phoneSchema.safeParse(phone);
+    if (!result.success) {
+      return {
+        token: false,
+        error: result.error.flatten(),
+      };
+    } else {
+      return {
+        token: true,
+      };
+    }
+  } else {
+    const result = tokenSchema.safeParse(token);
+    if (!result.success) {
+      return {
+        token: true,
+        error: result.error.flatten(),
+      };
+    } else {
+      redirect("/");
+    }
+  }
 }
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -2,28 +2,44 @@
 
 import Button from "@/components/button";
 import Input from "@/components/input";
-import { smsLogin } from "./actions";
 import { useFormState } from "react-dom";
+import { smsLogin } from "./actions";
 
-export default function SMSPage() {
-  const [state, dispatch] = useFormState(smsLogin, null);
+const initialState = {
+  token: false,
+  error: undefined,
+};
+
+export default function SMSLogin() {
+  const [state, dispatch] = useFormState(smsLogin, initialState);
   return (
     <div className="flex flex-col gap-10 px-6 py-8">
       <div className="flex flex-col gap-2 *:font-medium">
-        <h1 className="text-2xl">SMS Login</h1>
-        <p className="text-xl">Verify your phone number.</p>
+        <h1 className="text-2xl">SMS Log in</h1>
+        <h2 className="text-xl">Verify your phone number.</h2>
       </div>
       <form action={dispatch} className="flex flex-col gap-3">
-        <Input name="phone" type="text" placeholder="Phone number" required />
-        <Input
-          name="token"
-          type="number"
-          placeholder="Verification code"
-          required
-          min={100000}
-          max={999999}
-        />
-        <Button text="Verify" />
+        {!state.token && (
+          <Input
+            name="phone"
+            type="text"
+            placeholder="Phone number"
+            required
+            autoComplete="off"
+            errors={state.error?.formErrors}
+          />
+        )}
+        {state.token && (
+          <Input
+            name="token"
+            type="number"
+            placeholder="Verification code"
+            required
+            min={100000}
+            max={999999}
+          />
+        )}
+        <Button text={state.token ? "Verify Token" : "Send Verification SMS"} />
       </form>
     </div>
   );
