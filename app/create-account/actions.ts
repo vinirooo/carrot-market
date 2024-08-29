cc97037b5dae850936af703fe72a9a001c831579
vinirooo <vinirooo@gmail.com>
2024-08-29 23:49:06 +0900

    6-3 Transformation

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -2,7 +2,7 @@
 
 import { z } from "zod";
 
-const checkUsername = (username: string) => username.includes("admin");
+const checkUsername = (username: string) => !username.includes("admin");
 const checkConfirmPassword = ({
   password,
   confirmPassword,
@@ -11,18 +11,27 @@ const checkConfirmPassword = ({
   confirmPassword: string;
 }) => password === confirmPassword;
 
+const passwordRegex = new RegExp(
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*?[#?!@$%^&*-]).+$/,
+);
+
 const formSchema = z
   .object({
     username: z
       .string()
       .min(3, "username은 3자보다 길어야 합니다.")
       .max(20, "username은 20자보다 짧아야 합니다.")
+      .trim()
       .refine(checkUsername, "username은 admin을 포함할 수 없습니다."),
-    email: z.string().email(),
+    email: z.string().email().toLowerCase(),
     password: z
       .string()
       .min(6, "비밀번호는 6자 이상이어야 합니다.")
-      .max(20, "비밀번호는 20자 이하여야 합니다."),
+      .max(20, "비밀번호는 20자 이하여야 합니다.")
+      .regex(
+        passwordRegex,
+        "비밀번호는 대문자, 소문자, 숫자, 특수문자를 포함해야 합니다.",
+      ),
     confirmPassword: z.string(),
   })
   .refine(checkConfirmPassword, {
@@ -41,5 +50,7 @@ export async function createAccount(prevState: any, formData: FormData) {
   const result = formSchema.safeParse(data);
   if (!result.success) {
     return result.error.flatten();
+  } else {
+    console.log(result.data);
   }
 }
